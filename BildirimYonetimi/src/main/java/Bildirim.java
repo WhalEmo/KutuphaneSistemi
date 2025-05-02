0dd18700b60d7ec212f115ddee07369643eb9037
WhalEmo <emrullah64uygun@gmail.com>
2025-05-02 17:01:58 +0300

    Bildirim servisi

--- BildirimYonetimi/src/main/java/Bildirim.java
+++ BildirimYonetimi/src/main/java/Bildirim.java
@@ -1,2 +1,55 @@
+import java.sql.Timestamp;
+
 public class Bildirim {
+    private int ID;
+    private int KullaniciID;
+    private String Mesaj;
+    private Timestamp GonderimTarihi;
+    private String Tip;
+
+    public Bildirim(int kullaniciID, String mesaj, String Tip) {
+        KullaniciID = kullaniciID;
+        Mesaj = mesaj;
+        this.Tip = Tip;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getKullaniciID() {
+        return KullaniciID;
+    }
+
+    public void setKullaniciID(int kullaniciID) {
+        KullaniciID = kullaniciID;
+    }
+
+    public String getMesaj() {
+        return Mesaj;
+    }
+
+    public void setMesaj(String mesaj) {
+        Mesaj = mesaj;
+    }
+
+    public Timestamp getGonderimTarihi() {
+        return GonderimTarihi;
+    }
+
+    public void setGonderimTarihi(Timestamp gonderimTarihi) {
+        GonderimTarihi = gonderimTarihi;
+    }
+
+    public String getTip() {
+        return Tip;
+    }
+
+    public void setTip(String tip) {
+        Tip = tip;
+    }
 }
--- BildirimYonetimi/src/main/java/BildirimServisi.java
+++ BildirimYonetimi/src/main/java/BildirimServisi.java
@@ -1,2 +1,20 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class BildirimServisi {
+
+    public boolean BildirimKayit(Bildirim bildirim){
+        String sql = "INSERT INTO Bildirim (KullaniciID, Mesaj, Tip) VALUES (?, ?, ?)";
+        try {
+            Connection baglanti = VeriTabaniBaglantisi.Baglanti();
+            PreparedStatement sorgu = baglanti.prepareStatement(sql);
+            sorgu.setInt(1,bildirim.getKullaniciID());
+            sorgu.setString(2,bildirim.getMesaj());
+            sorgu.setString(3,bildirim.getTip());
+        }catch (Exception e){
+            System.out.println("Hata: "+e.getMessage());
+        }
+        return false;
+    }
+
 }
